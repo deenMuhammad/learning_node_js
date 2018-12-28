802c14e2525e1b09e9c9f4fcf26f303f224f355c
Dinmukhammad Rakhimov <dinmukhammadrakhimov@Dinmukhammads-MacBook-Pro.local>
2018-12-28 10:10:32 +0900

    Stated Learning Node JS

---
+++ filesystem/networking/net-watcher.js
@@ -0,0 +1,21 @@
+'use strict'
+
+const net = require('net');
+const fs = require('fs');
+const filename = process.argv[2];
+
+if(!filename){
+    throw Error("Valid Filename must be included!");
+}
+net.createServer(connection=>{
+    console.log("Subscriber Connected");
+    connection.write(`Now watching ${filename} for changes...\n`);
+
+    const watcher = fs.watch(filename, ()=>{
+        connection.write(`File changed: ${new Date()}\n`)
+    });
+    connection.on('close', ()=>{
+        console.log("Subscriber Disconnected");
+        watcher.close();
+    })
+}).listen(60300, ()=>{console.log("Listening for Subscribers...")});
